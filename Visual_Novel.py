78a654f5e1fc5203f5d71cdc918781a40f2156df
Vazymolo92 <53569961+Vazymolo92@users.noreply.github.com>
2023-04-17 01:07:13 +0200

    Update Visual_Novel.py

--- Visual_Novel.py
+++ Visual_Novel.py
@@ -1,109 +1,117 @@
 from tkinter import *
+import classe_arbre_binaire
 
-class ArbreBinaire:
-    """classe permetant de simuler un arbre binaire"""
-    def __init__(self, val=None):
-        self.valeur=val
-        self.enfant_gauche=None
-        self.enfant_droit=None
+# création d'un arbre pour acceuillir l'histoire sous la forme ["Storyline","Question", "ChoixA", "Choix"B]   
+
+# Création de la racine de l'arbre
+arbre = classe_arbre_binaire.ArbreBinaire(["Vous vous réveillez sous le fracas des bombardements.", "Que faire ?", "Demi-Tour", "On s'en fiche"])
+arbre.insert_gauche(["Vous faites demi-tour mais vous ne voyez pas Billy.", "Où chercher ?", "Chercher à la maison", "Chercher à la cité"]) # B_node
+arbre.insert_droit(['Vous arrivez à rentrer en France Victoire.', "Que faire ?", "Non", "Oui"]) # C_node
+
+# Récupération du noeud B
+B_node = arbre.get_gauche() 
+B_node.insert_gauche(["Vous cherchez à la maison mais Billy n'y est pas.", "Que faire ?", "Continuer de chercher", "Aller à la cité"]) # D_node
+B_node.insert_droit(["Vous décidez d'aller à la cité et retrouvez Billy dans un abri.", "Que faire ?", "Rester caché", "Sortir pour chercher de l'aide"]) # E_node
+
+# Récupération du noeud C
+C_node = arbre.get_droit()
+C_node.insert_gauche(["Vous décidez de continuer seul et vous tombez sur un groupe de soldats ennemis.", "Que faire ?", "Fuir", "Attaquer"]) # F_node
+
+# Récupération du noeud D et ajout des choix pour le noeud F
+D_node = B_node.get_gauche()
+D_node.insert_gauche(["Vous continuez de chercher mais en vain.", "Que faire ?", "Rentrer chez vous", "Aller à la cité"]) # F_node
+D_node.insert_droit(["Vous décidez d'aller à la cité et croisez un groupe de résistants.", "Que faire ?", "Les rejoindre", "Continuer seul"]) # G_node
+
+# Récupération du noeud G et ajout des choix pour le noeud H
+G_node = D_node.get_droit()
+G_node.insert_gauche(["Vous rejoignez les résistants et participez à une mission pour saboter les lignes ennemies.", "Que faire maintenant ?", "Rentrer chez vous", "Continuer à aider la résistance"]) # H_node
+G_node.insert_droit(["Vous décidez de continuer seul et vous tombez sur un groupe de soldats ennemis.", "Que faire ?", "Fuir", "Attaquer"]) # I_node
+
+# Récupération du noeud E et ajout des choix pour le noeud J
+E_node = B_node.get_droit()
+E_node.insert_gauche(["Vous décidez de rester caché mais les provisions s'épuisent.", "Que faire ?", "Sortir pour chercher de l'aide", "Continuer à attendre"]) # J_node
+E_node.insert_droit(["Vous décidez de sortir pour chercher de l'aide et croisez un groupe de résistants.", "Que faire ?", "Les rejoindre", "Continuer seul"]) # K_node
+
+# Récupération du noeud F et ajout des choix pour les noeuds G et H
+F_node = C_node.get_gauche()
+F_node.insert_gauche(["Vous réussissez à fuir et trouvez refuge dans une petite ville.", "Que faire ?", "Rejoindre la résistance", "Rester caché"]) # G_node
+F_node.insert_droit(["Vous attaquez avec succès les soldats ennemis et continuez votre mission.", "Que faire ?", "Chercher de l'aide", "Continuer seul"]) # I_node
+
+# Récupération du noeud G et ajout des choix pour les noeuds de fin de l'histoire
+G_node = F_node.get_gauche()
+G_node.insert_gauche(["Vous rejoignez la résistance et participez à plusieurs missions réussies.", None, None, None])
+G_node.insert_droit(["Vous restez caché jusqu'à la fin de la guerre, mais avec des souvenirs traumatisants de la guerre.", None, None, None])
+
+# Récupération du noeud H et ajout des fins pour les noeuds de fin de l'histoire
+H_node = G_node.get_gauche()
+H_node.insert_gauche(["Vous êtes finalement rentré chez vous, fier de votre contribution à la libération de votre pays.", None, None, None])
+H_node.insert_droit(["Vous avez continué à aider la résistance jusqu'à la fin de la guerre.", None, None, None])
+
+# Récupération du noeud I et ajout des fins pour les noeuds de fin de l'histoire
+I_node = G_node.get_droit()
+I_node.insert_gauche(["Vous avez réussi à fuir et êtes finalement rentré chez vous, mais avec des souvenirs traumatisants de la guerre.", None, None, None])
+I_node.insert_droit(["Vous avez attaqué avec succès les soldats ennemis et êtes devenu un héros de la résistance.", None, None, None])
+
+# Récupération du noeud J et ajout des fins pour les noeuds de fin de l'histoire
+J_node = E_node.get_gauche()
+J_node.insert_gauche(["Vous êtes finalement sorti pour chercher de l'aide et avez réussi à rejoindre la résistance.", None, None, None])
+J_node.insert_droit(["Vous êtes resté caché jusqu'à la fin de la guerre, mais avec des souvenirs traumatisants de la guerre.", None, None, None])
+
+# Récupération du noeud K et ajout des choix pour le noeud L
+K_node = E_node.get_droit()
+K_node.insert_droit(["Vous décidez de continuer seul et vous tombez sur un groupe de soldats ennemis.", "Que faire ?", "Fuir", "Attaquer"]) # L_node
+
+# Récupération du noeud L et ajout des fins pour les noeuds de fin de l'histoire
+L_node = K_node.get_droit()
+L_node.insert_gauche(["Vous avez réussi à fuir et êtes finalement rentré chez vous, mais avec des souvenirs traumatisants de la guerre.", None, None, None])
+L_node.insert_droit(["Vous avez attaqué avec succès les soldats ennemis et êtes devenu un héros de la résistance.", None, None, None])
+
+# Création d'une fenêtre Tkinter
+window = Tk()
+
+# Définition du titre de la fenêtre
+window.title("Jeu a Choix")
+
+# Définition des dimensions de la fenêtre
+window.geometry("540x720")
+
+# Définition d'une zone de text histoire avec une variable pour la modifier           
+global storyline
+storyline = StringVar()
+Label(window, textvariable=storyline,pady=10).pack()
+storyline.set("Storyline")
+
+# Définition d'une zone de text question avec une variable pour la modifier 
+global question
+question = StringVar()
+Label(window, textvariable=question,pady=10).pack()
+storyline.set("Question")
+
+# Définition d'une frame pour acceuillir les boutons 
+global frame
+frame = Frame(window)
+frame.pack(side="top")
+
+#définition des boutons de choix
+global btnA
+global btnB
+btnA = Button(frame, text="videA", relief=GROOVE, width=5)
+btnB = Button(frame, text="videB", relief=GROOVE, width=5)
+
+def jeu(arbre):
+    """fonction permettant de parcourir l'arbre binaire et d'effectuer une partie"""
+    global btnA
+    global btnB
+    btnA.destroy()
+    btnB.destroy()
+    storyline.set(arbre.get_valeur()[0])
+    question.set(arbre.get_valeur()[1])
+    if arbre.get_valeur()[1] != None and arbre.get_valeur()[2] != None:    
+        btnA = Button(frame, text=arbre.get_valeur()[2], relief=GROOVE, width=20, command = lambda: jeu(arbre.get_gauche()))
+        btnB = Button(frame, text=arbre.get_valeur()[3], relief=GROOVE, width=20, command = lambda: jeu(arbre.get_droit()))
+        btnA.pack(side="top")
+        btnB.pack(side="bottom")
         
-    def insert_gauche(self,valeur):
-        if self.enfant_gauche==None:
-            self.enfant_gauche=ArbreBinaire(valeur)
-        else:
-            arbre_gauche=ArbreBinaire(valeur)
-            arbre_gauche.enfant_gauche=self.enfant_gauche
-            self.enfant_gauche=arbre_gauche
-            
-    def insert_droit(self,valeur):
-        if self.enfant_droit==None:
-            self.enfant_droit=ArbreBinaire(valeur)
-        else:
-            arbre_droit=ArbreBinaire(valeur)
-            arbre_droit.enfant_droit=self.enfant_droit
-            self.enfant_droit=arbre_droit
-            
-    def get_valeur(self):
-        return self.valeur
-    
-    def get_droit(self):
-        return self.enfant_droit
-    
-    def get_gauche(self):
-        return self.enfant_gauche
-    
-    def __str__(self):
-        return f'({self.valeur},{self.enfant_gauche},{self.enfant_droit})' 
-    
-def binary_tree():
-    """fonction créant un arbre binaire pour y implémenter l'histoire"""
-    #sous la forme (background, storyline, choixA, choixB)
-    racine=ArbreBinaire((16,"Amine se réveille sous les bruits sourds des bombardement nazis et fuit à toute vitesse, cependant il a oublié Billy","Demi-Tour", "On s'en fiche"))
-    racine.insert_gauche((10,"Mais où allez vous le cherchez ?", "La cité voisine", "la maison"))
-    racine.insert_droit((10,"Vous quittez les lieux mais les nazis vous attrape, vous êtes emprisonnés", None, None))
+jeu(arbre)
 
-    demiTour=racine.get_gauche()
-    demiTour.insert_gauche((15,"Vous atteignez la cité voisine, Andrew Tate chef de la rebellion vous propose de le rejoindre", "rejoindre la rebellion", "continuer en solo"))
-    demiTour.insert_droit((6,"Vous arrivez à la maison mais celle-ci à est perquisitionné par les nazis", "coopérer", "résister"))
-    
-    rejoindreRebellion=racine.demiTour.get_gauche()
-    rejoindreRebellion.insert_gauche((18,"Vous rejoignez la rebellion, on vous propose de participer à la prochaine opération", "participer", "rester au QG"))
-    rejoindreRebellion.insert_droit((18,"Vous souhaitez partir mais Andrew ne pux pas vous laisser partir et vous élimine", None, None))
-    
-    return racine
-
-def parcours(arbre):
-    pass
-    
-
-def creation_fenetre():
-    # Creer une 1ere fenetre
-    window = Tk()
-
-    textA="choiceA"
-    textB="choiceB"
-    storyline="Storyline"
-    background=1
-
-    # Personnaliser cette fenetre
-    window.title("Jeu à Choix")
-    window.geometry("540x720")
-    window.minsize(540, 360)
-    window.config(background='#7D9479')
-    
-    # Creer la frame
-    frame=Frame(window, bg='#7D9479')
-
-    # Creer 1er image (dans la frame)
-    width = 256*2
-    height = 192*2
-    image = PhotoImage(file="background1.png")
-
-    canvas = Canvas(frame, width=width, height=height, bg='black', bd=0, highlightthickness=0)
-    canvas.create_image(width/2, height/2, image=image)
-    canvas.grid(row=0, column=0, sticky=N)
-
-    # Creer une sous-frame
-    down_frame = Frame(frame, bg='#7D9479')
-
-    # Ajouter 1er texte (dans la sous-frame)
-    label_storyline = Label(down_frame, text=storyline, font=("Helvetica",15), bg='#7D9479', fg='black')
-    label_storyline.pack()
-
-    # Ajouter le bouton du choix A (dans la sous-frame)
-    play_buttonA = Button(down_frame, text=textA, font=("Courrier",15), bg='#3C6036', fg='white')
-    play_buttonA.pack()
-
-    # Ajouter le bouton du choix B (dans la sous-frame)
-    play_buttonB = Button(down_frame, text=textB, font=("Courrier",15), bg='#3C6036', fg='white')
-    play_buttonB.pack()
-
-    #Placer la sous-frame dans la frame principale
-    down_frame.grid(row=1, column=0, sticky=S)
-
-    # Afficher la Frame
-    frame.pack(expand=YES)
-
-    # Afficher
-    window.mainloop()
-
+window.mainloop()
