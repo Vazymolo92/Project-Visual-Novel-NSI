f46c199eb2b5cc706f435b32920d2a0821723d0d
Vazymolo92 <53569961+Vazymolo92@users.noreply.github.com>
2023-04-18 16:45:03 +0200

    Update Visual_Novel.py

--- Visual_Novel.py
+++ Visual_Novel.py
@@ -4,67 +4,64 @@ import classe_arbre_binaire
 # création d'un arbre pour acceuillir l'histoire sous la forme ["Storyline","Question", "ChoixA", "Choix"B]   
 
 # Création de la racine de l'arbre
-arbre = classe_arbre_binaire.ArbreBinaire(["Vous vous réveillez sous le fracas des bombardements.", "Que faire ?", "Demi-Tour", "On s'en fiche"])
-arbre.insert_gauche(["Vous faites demi-tour mais vous ne voyez pas Billy.", "Où chercher ?", "Chercher à la maison", "Chercher à la cité"]) # B_node
-arbre.insert_droit(['Vous arrivez à rentrer en France Victoire.', "Que faire ?", "Non", "Oui"]) # C_node
+arbre = classe_arbre_binaire.ArbreBinaire(["POLOGNE 1940 : Vous vous réveillez sous le fracas des bombardements.", "Que faire ?", "Fuir", "Rassembler vos affaires"])
+arbre.insert_gauche(["Vous partez en direction de l'Est Mais rencontrez un régiment Nazi.", "Que faire ?", "Les attaquer", "Essayer de ne pas se faire remarquer"]) # B_node
+arbre.insert_droit(['Vous récuperez des soupes en conserves puis pliez bagages.', "Que faire ?", "Aller chercher votre ami", "Fuire seul"]) # C_node
 
 # Récupération du noeud B
 B_node = arbre.get_gauche() 
-B_node.insert_gauche(["Vous cherchez à la maison mais Billy n'y est pas.", "Que faire ?", "Continuer de chercher", "Aller à la cité"]) # D_node
-B_node.insert_droit(["Vous décidez d'aller à la cité et retrouvez Billy dans un abri.", "Que faire ?", "Rester caché", "Sortir pour chercher de l'aide"]) # E_node
+B_node.insert_gauche(["Vous attaquez les soldats et parvenez à récuperer un fusil.", "Continuer à combattre?", "Se rendre", "Continuer le combat"]) # D_node
+B_node.insert_droit(["Vous essayez de passer innaperçu mais les nazis vous ont reperer.", "Que faire ?", "Se faire passer pour un russe", "Les insulter, après tout vous n'avez plus rien à perdre"]) # E_node
 
 # Récupération du noeud C
 C_node = arbre.get_droit()
-C_node.insert_gauche(["Vous décidez de continuer seul et vous tombez sur un groupe de soldats ennemis.", "Que faire ?", "Fuir", "Attaquer"]) # F_node
+C_node.insert_gauche(["Vous Partez vers la ville pour retrouver votre ami.", "Où chercher ?", "Sa maison", "La cité voisine"]) # F_node
+C_node.insert_droit(["Vous n'avez aucun sens de l'amitié et décidez de faire cavalier seul.", "Par quel moyen allez vous fuir ?", "En bateau", "En train"]) # G_node
 
 # Récupération du noeud D et ajout des choix pour le noeud F
 D_node = B_node.get_gauche()
-D_node.insert_gauche(["Vous continuez de chercher mais en vain.", "Que faire ?", "Rentrer chez vous", "Aller à la cité"]) # F_node
-D_node.insert_droit(["Vous décidez d'aller à la cité et croisez un groupe de résistants.", "Que faire ?", "Les rejoindre", "Continuer seul"]) # G_node
-
-# Récupération du noeud G et ajout des choix pour le noeud H
-G_node = D_node.get_droit()
-G_node.insert_gauche(["Vous rejoignez les résistants et participez à une mission pour saboter les lignes ennemies.", "Que faire maintenant ?", "Rentrer chez vous", "Continuer à aider la résistance"]) # H_node
-G_node.insert_droit(["Vous décidez de continuer seul et vous tombez sur un groupe de soldats ennemis.", "Que faire ?", "Fuir", "Attaquer"]) # I_node
+D_node.insert_gauche(["Les nazis vous capture et vous amène à leur colonel.", "Que faire ?", "Le supplier de vous épargner", "Se faire passer pour un tchetchene"]) # H_node
+D_node.insert_droit(["Vous recevez une balle à l'épaule et battez en retraite, vous rencontrez un marchand.", "Que faire ?", "Lui voler ses médicaments", "Tenter de s'associer à lui"]) # I_node
 
 # Récupération du noeud E et ajout des choix pour le noeud J
 E_node = B_node.get_droit()
-E_node.insert_gauche(["Vous décidez de rester caché mais les provisions s'épuisent.", "Que faire ?", "Sortir pour chercher de l'aide", "Continuer à attendre"]) # J_node
+E_node.insert_gauche(["Vous bafouillez des sons russe, les Nazis prennent peur et vous laisse.", "Que faire ?", "Rejoindre un village", "Continuer votre chemin"]) # J_node
 E_node.insert_droit(["Vous décidez de sortir pour chercher de l'aide et croisez un groupe de résistants.", "Que faire ?", "Les rejoindre", "Continuer seul"]) # K_node
 
 # Récupération du noeud F et ajout des choix pour les noeuds G et H
 F_node = C_node.get_gauche()
-F_node.insert_gauche(["Vous réussissez à fuir et trouvez refuge dans une petite ville.", "Que faire ?", "Rejoindre la résistance", "Rester caché"]) # G_node
-F_node.insert_droit(["Vous attaquez avec succès les soldats ennemis et continuez votre mission.", "Que faire ?", "Chercher de l'aide", "Continuer seul"]) # I_node
+F_node.insert_gauche(["Vous réussissez à fuir et trouvez refuge dans une petite ville.", "Que faire ?", "Rejoindre la résistance", "Rester caché"]) # L_node
+F_node.insert_droit(["Vous attaquez avec succès les soldats ennemis et continuez votre mission.", "Que faire ?", "Chercher de l'aide", "Continuer seul"]) # M_node
 
 # Récupération du noeud G et ajout des choix pour les noeuds de fin de l'histoire
-G_node = F_node.get_gauche()
-G_node.insert_gauche(["Vous rejoignez la résistance et participez à plusieurs missions réussies.", None, None, None])
-G_node.insert_droit(["Vous restez caché jusqu'à la fin de la guerre, mais avec des souvenirs traumatisants de la guerre.", None, None, None])
+G_node = C_node.get_droit()
+G_node.insert_gauche(["Vous rejoignez la résistance et participez à plusieurs missions réussies.", "FIN", None, None])
+G_node.insert_droit(["Vous restez caché jusqu'à la fin de la guerre, mais avec des souvenirs traumatisants de la guerre.", "FIN", None, None])
 
 # Récupération du noeud H et ajout des fins pour les noeuds de fin de l'histoire
-H_node = G_node.get_gauche()
-H_node.insert_gauche(["Vous êtes finalement rentré chez vous, fier de votre contribution à la libération de votre pays.", None, None, None])
-H_node.insert_droit(["Vous avez continué à aider la résistance jusqu'à la fin de la guerre.", None, None, None])
+H_node = D_node.get_gauche()
+H_node.insert_gauche(["Vous êtes finalement rentré chez vous, fier de votre contribution à la libération de votre pays.", "FIN", None, None])
+H_node.insert_droit(["En parlant tchechene vous vous êtes transformé en Super Saiyan et les avez tués.", "VICTOIRE", None])
 
 # Récupération du noeud I et ajout des fins pour les noeuds de fin de l'histoire
-I_node = G_node.get_droit()
+I_node = D_node.get_droit()
 I_node.insert_gauche(["Vous avez réussi à fuir et êtes finalement rentré chez vous, mais avec des souvenirs traumatisants de la guerre.", None, None, None])
-I_node.insert_droit(["Vous avez attaqué avec succès les soldats ennemis et êtes devenu un héros de la résistance.", None, None, None])
+I_node.insert_droit(["Vous avez attaqué avec succès les soldats ennemis et êtes devenu un héros de la résistance.", "FIN", None, None])
 
 # Récupération du noeud J et ajout des fins pour les noeuds de fin de l'histoire
 J_node = E_node.get_gauche()
-J_node.insert_gauche(["Vous êtes finalement sorti pour chercher de l'aide et avez réussi à rejoindre la résistance.", None, None, None])
-J_node.insert_droit(["Vous êtes resté caché jusqu'à la fin de la guerre, mais avec des souvenirs traumatisants de la guerre.", None, None, None])
+J_node.insert_gauche(["Vous êtes finalement sorti pour chercher de l'aide et avez réussi à rejoindre la résistance.", "FIN", None, None])
+J_node.insert_droit(["Vous êtes resté caché jusqu'à la fin de la guerre, mais avec des souvenirs traumatisants de la guerre.", "FIN", None, None])
 
 # Récupération du noeud K et ajout des choix pour le noeud L
 K_node = E_node.get_droit()
 K_node.insert_droit(["Vous décidez de continuer seul et vous tombez sur un groupe de soldats ennemis.", "Que faire ?", "Fuir", "Attaquer"]) # L_node
+K_node.insert_droit(["Vous trouvez un marchand d'arme.", "Que faire ?", "Le voler", "Attaquer"])
 
 # Récupération du noeud L et ajout des fins pour les noeuds de fin de l'histoire
-L_node = K_node.get_droit()
-L_node.insert_gauche(["Vous avez réussi à fuir et êtes finalement rentré chez vous, mais avec des souvenirs traumatisants de la guerre.", None, None, None])
-L_node.insert_droit(["Vous avez attaqué avec succès les soldats ennemis et êtes devenu un héros de la résistance.", None, None, None])
+L_node = F_node.get_gauche()
+L_node.insert_gauche(["Vous avez réussi à fuir et êtes finalement rentré chez vous, mais avec des souvenirs traumatisants de la guerre.", "VICTOIRE", None, None])
+L_node.insert_droit(["Vous avez attaqué avec succès les soldats ennemis et êtes devenu un héros de la résistance.", "VICTOIRE", None, None])
 
 # Création d'une fenêtre Tkinter
 window = Tk()
